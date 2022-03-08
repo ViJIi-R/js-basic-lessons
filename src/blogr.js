d29304261f4a7e288ce818becc1b8be921edb2e4
Vijaysri R <vija3017@global.chainsys.com>
2022-03-08 09:04:24 +0530

    Base Page updated

---
+++ src/blogr.js
@@ -0,0 +1,19 @@
+const hamburger = document.querySelector('.hero__menu img')
+const mobileMenu = document.querySelector('.mobile__menu')
+
+const btnClickHandler = () => {
+  console.log('working...')
+
+  if (hamburger.classList.contains('change')) {
+    hamburger.classList.toggle('change')
+    hamburger.setAttribute('src', './images/icon-hamburger.svg')
+    mobileMenu.classList.remove('mobile-active')
+  } else {
+    hamburger.classList.toggle('change')
+    hamburger.setAttribute('src', './images/icon-close.svg')
+    hamburger.setAttribute('alt', 'close button')
+    mobileMenu.classList.add('mobile-active')
+  }
+}
+
+hamburger.addEventListener('click', () => btnClickHandler())
